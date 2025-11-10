0284ccc498b5079df3905896c0e6c2ffd355c470
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2025-11-10 19:13:06 +0100

    small fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -113,69 +113,12 @@ require("lazy").setup({
             keymap = { preset = 'enter' },
 
             appearance = {
-                nerd_font = true,
                 use_nvim_cmp_as_default = true,
                 nerd_font_variant = 'mono'
             },
             signature = { enabled = true }
         }
     },
-
-    -- {
-    -- "hrsh7th/nvim-cmp",
-    -- dependencies = {
-    --   "hrsh7th/cmp-nvim-lsp",
-    --   "L3MON4D3/LuaSnip",             -- Snippet engine
-    --   "saadparwaiz1/cmp_luasnip",     -- Snippet completion
-    --   "hrsh7th/cmp-buffer",           -- Buffer words
-    --   "hrsh7th/cmp-path",             -- File paths
-    --   "rafamadriz/friendly-snippets", -- Predefined snippets
-    -- },
-    -- config = function()
-    --   local cmp = require("cmp")
-    --   local luasnip = require("luasnip")
-    --
-    --   require("luasnip.loaders.from_vscode").lazy_load()
-    --
-    --   cmp.setup({
-    --     snippet = {
-    --       expand = function(args)
-    --         luasnip.lsp_expand(args.body)
-    --       end,
-    --     },
-    --     mapping = cmp.mapping.preset.insert({
-    --       ["<Tab>"] = cmp.mapping(function(fallback)
-    --         if cmp.visible() then
-    --           cmp.select_next_item()
-    --         elseif luasnip.expand_or_jumpable() then
-    --           luasnip.expand_or_jump()
-    --         else
-    --           fallback()
-    --         end
-    --       end, { "i", "s" }),
-    --
-    --       ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --         if cmp.visible() then
-    --           cmp.select_prev_item()
-    --         elseif luasnip.jumpable(-1) then
-    --           luasnip.jump(-1)
-    --         else
-    --           fallback()
-    --         end
-    --       end, { "i", "s" }),
-    --
-    --       ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    --       ["<C-Space>"] = cmp.mapping.complete(),
-    --     }),
-    --     sources = cmp.config.sources({
-    --       { name = "nvim_lsp" },
-    --       { name = "luasnip" },
-    --       { name = "buffer" },
-    --       { name = "path" },
-    --     }),
-    --   })
-    -- end,
-    -- },
 
     -- nvim-treesitter
     {
