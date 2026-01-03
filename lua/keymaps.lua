40ff554177363acb8169468f49f8fcbd99ff11eb
Maciej Grześkowiak <maciej.grzes07@gmail.com>
2026-01-03 20:53:46 +0100

    whatever man

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -35,32 +35,12 @@ map('n', '<C-Down>', ':resize +2<CR>', opts)
 map('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 map('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
-map('n', 'h', 'i', opts)
--- arrow like hjkl
-map("n", "i", "k", opts) -- up
-map("n", "j", "h", opts) -- left
-map("n", "k", "j", opts) -- down
-
 -----------------
 -- Visual mode --
 -----------------
 
 map('v', '<', '<gv', opts)
 map('v', '>', '>gv', opts)
-
-map("v", "i", "k", opts)
-map("v", "j", "h", opts)
-map("v", "k", "j", opts)
-map("v", "l", "l", opts)
-
----------------------------
--- Operator pending mode --
----------------------------
-
-map("o", "i", "k", opts)
-map("o", "j", "h", opts)
-map("o", "k", "j", opts)
-map("o", "l", "l", opts)
 
 
 -------------------
